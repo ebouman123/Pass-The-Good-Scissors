1b0d62f5d973746ee59a29d476a03aae609df966
Evan Bouman <evan.bouman@gmail.com>
2024-10-28 11:31:17 -0500

    Added SQL to database.sql to create the necessary tables

--- database.sql
+++ database.sql
@@ -1,9 +1,22 @@
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
+);
+
+CREATE TABLE "project" (
+    "id" SERIAL PRIMARY KEY,
+    "projectName" VARCHAR (80) UNIQUE NOT NULL,
+    "projectComment" VARCHAR (400),
+    "finishedQuiltName" VARCHAR (80) UNIQUE,
+	"finishedQuiltComment" VARCHAR (400),
+	"user_id" INTEGER REFERENCES "user"
+);
+
+CREATE TABLE "fabric" (
+    "id" SERIAL PRIMARY KEY,
+    "fabricName" VARCHAR (80) UNIQUE NOT NULL,
+    "fabricComment" VARCHAR (400),
+    "fabricLink" VARCHAR (200),
+	"project_id" INTEGER REFERENCES "project"
 );
