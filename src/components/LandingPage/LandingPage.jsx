bbb7c6da23880cd930c14c0026602d13668cb924
Evan Bouman <evan.bouman@gmail.com>
2024-11-03 21:01:53 -0600

    Initial work on landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -21,9 +21,44 @@ function LandingPage() {
     <Box>
       <Box>
         <Typography variant="h4">
-          Pass The Good Scissors! is a tool that helps you choose your fabric
-          for your next quilting project.
+          Simplify Your Quilting Journey:
+          <Typography variant="h5">
+            Say goodbye to guesswork! With "Pass the Good Scissors," planning
+            your next quilt has never been easier.
+          </Typography>
         </Typography>
+      </Box>
+      <Box>
+        <Box sx={{ border: 3 }}>
+          <Typography variant="h6">
+            Visualize Your Quilt:
+            <Typography variant="subtitle1">
+              Upload your favorite fabric images and easily arrange them on a
+              dynamic grid to see how they work together
+            </Typography>
+          </Typography>
+          <img src="Manage Fabrics.png" height="800" />
+        </Box>
+        <Box sx={{ border: 3 }}>
+          <Typography variant="h6">
+            Design with Confidence:
+            <Typography variant="subtitle1">
+              Experiment with different fabric combinations to create the
+              perfect quilt design before you start cutting
+            </Typography>
+          </Typography>
+          <img src="Planning Tool.png" height="800" />
+        </Box>
+        <Box sx={{ border: 3 }}>
+          <Typography variant="h6">
+            Download Your Creations:
+            <Typography variant="subtitle1">
+              Once you’ve crafted your ideal pattern, download an image of your
+              design to guide your project
+            </Typography>
+          </Typography>
+          <img src="Example Export.png" height="800" />
+        </Box>
       </Box>
     </Box>
   );
